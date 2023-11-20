e0d3b2123e44886f876e81ec464657b7b6851d41
liam <geffc1454@gmail.com>
2023-11-21 00:25:43 +0800

    [update]

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,17 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "Batman"
+--config.color_scheme = "Batman"
+config.color_scheme = "OneDark (base16)"
+
 config.enable_wayland = true
+config.enable_tab_bar = false
 config.automatically_reload_config = false
-config.font = "Comic Mono"
+config.window_background_opacity = 0.5
+config.font = wezterm.font_with_fallback({
+	"Comic Mono",
+	"CaskaydiaCove Nerd Font",
+	"Yozai",
+})
 
 return config
