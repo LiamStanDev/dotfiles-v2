97f7bf012d6cabf2f57eb8f6985cb65f5083ae9b
liam <geffc1454@gmail.com>
2023-12-02 10:28:49 +0800

    [fix] nvim-ufo sidebar icon

--- nvim/.config/nvim/lua/editor/nvim-ufo.lua
+++ nvim/.config/nvim/lua/editor/nvim-ufo.lua
@@ -1,16 +1,40 @@
 return function()
-	vim.o.foldcolumn = "1" -- '0' is not bad
+	vim.o.foldcolumn = "5" -- '0' is not bad
 	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-	vim.o.foldlevelstart = 99
+	vim.o.foldlevelstart = 3
+	vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 	vim.o.foldenable = true
 
-	-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-	vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-	vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+	local ftMap = {
+		vim = "indent",
+		python = { "indent" },
+		git = "",
+	}
 
 	require("ufo").setup({
+		open_fold_hl_timeout = 150,
+		close_fold_kinds = { "imports", "comment" },
+		preview = {
+			win_config = {
+				border = { "", "─", "", "", "", "─", "", "" },
+				winhighlight = "Normal:Folded",
+				winblend = 0,
+			},
+			mappings = {
+				scrollU = "<C-u>",
+				scrollD = "<C-d>",
+				jumpTop = "[",
+				jumpBot = "]",
+			},
+		},
 		provider_selector = function(bufnr, filetype, buftype)
-			return { "treesitter", "indent" }
+			return ftMap[filetype] or { "treesitter", "indent" }
 		end,
 	})
+	-- press z the which key will show more information
+	-- za toggle fold
+	vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+	vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+	vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+	vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
 end
