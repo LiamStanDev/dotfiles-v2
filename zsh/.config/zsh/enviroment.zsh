668ee37df253c14639fd4a145614a362f0bd0c07
liam <geffc1454@gmail.com>
2023-12-10 10:02:44 +0800

    [update] move zsh export to hyprland env

--- zsh/.config/zsh/enviroment.zsh
+++ zsh/.config/zsh/enviroment.zsh
@@ -1,13 +1,6 @@
-# key setting
-export NO_AT_BRIDGE=1
-export SDL_VIDEODRIVER=wayland
-export _JAVA_AWT_WM_NONREPARENTING=wayland
-export GDK_BACKEND="wayland,x11"
 # conda
 export CONDA_AUTO_ACTIVATE_BASE=true
 # XDG
-export XDG_CURRENT_DESKTOP=Hyprland
-export XDG_SESSION_DESKTOP=Hyprland
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
@@ -21,8 +14,3 @@ export XDG_DOCUMENTS_DIR="$HOME/Documents"
 export XDG_MUSIC_DIR="$HOME/Music"
 export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_VIDEOS_DIR="$HOME/Videos"
-# input source
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-export GLFW_IM_MODULE=ibus
