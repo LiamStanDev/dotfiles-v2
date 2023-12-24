eb1ee037288e21ae8b9d79ba2d01a24ae601ccc7
liam <geffc1454@gmail.com>
2023-12-24 19:49:18 +0800

    [new] add gitapi notification

---
+++ waybar/.config/waybar/scripts/github.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
+
+if [ "$notifications" -gt 0 ]; then
+    echo "$notifications"
+else
+    echo "0"
+fi
